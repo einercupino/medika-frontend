5801441f94fb056f688a13770131984de77c317b
Einer Cupino <ecupino@my.centennialcollege.ca>
2024-04-02 16:35:07 -0400

    port changed to 3500

--- src/pages/PatientAssessment.jsx
+++ src/pages/PatientAssessment.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React,  { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const PatientAssessment = () => {
+  const navigate = useNavigate();
   return (
     <div>PatientAssessment</div>
   )
