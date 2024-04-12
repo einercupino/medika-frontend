276c34504ae8d4b9f50620f47ab77a4623b1d370
Jasper Jan Tan <jasperjantan1996@gmail.com>
2024-04-11 21:19:36 -0400

    Added

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -1,6 +1,7 @@
+const expect = require('chai').expect;
+
 describe('Dummy Test', () => {
     it('should pass', () => {
-      expect(true).toBeTruthy();
+        expect(true).to.be.true;
     });
-  });
-  
+});
